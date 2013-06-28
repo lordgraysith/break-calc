a13d04ebfb36f947326cef104da6c6be38c73248
Michael Graybeal <michael@thegraybeals.com>
2013-06-28 16:20:26 -0600

    it works!

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,71 @@
 $(function(){
-	var start
-	, now = new Date()
-	, findTheEnd;
+	var startInput
+	, $startInput
+	, $duration
+	, endInput
+	, startDate
+	, findTheEnd
+	, tzOffset
+	, setInputDate
+	, startDateChange
+	, thirtyFourHours = 34 * 60 * 60 * 1000;
 
-	findTheEnd = function(){
-		var milliseconds = 0;
-		
+	findTheEnd = function(startDate){
+		var milliseconds = 0
+		, endDate;
+
+		//initialize the end date as the same time
+		endDate = new Date(startDate.getTime());
+
+		//if it's past one AM, move to next day
+		if(startDate.getHours() > 1 || (startDate.getHours() === 1 && startDate.getMinutes() > 0)){
+			endDate.setDate(startDate.getDate() + 1);
+		}
+		//now advance to 5 AM the next day
+		endDate.setDate(endDate.getDate() + 1);
+		endDate.setHours(5);
+		endDate.setMinutes(0);
+
+		//see if the difference is 34 hours or more
+		if(endDate - startDate >= thirtyFourHours){
+			return endDate;
+		}//if not, then just 34 hours from start will do
+		else{
+			return new Date(startDate.getTime() + thirtyFourHours);
+		}
 	};
 
-	start = $('#break-start')[0];
-	start.valueAsNumber = now.getTime() - (now.getTimezoneOffset() * 60000);
+	setInputDate = function(input, date){
+		input.valueAsNumber = date.getTime() - (date.getTimezoneOffset() * 60000);
+	};
+
+	startDateChange = function(){
+		var endDate
+		, duration
+		, hours
+		, minutes;
+		startDate = new Date(startInput.valueAsNumber + (tzOffset * 60000));
+		endDate = findTheEnd(startDate);
+		setInputDate(endInput, endDate);
+		duration = endDate - startDate;
+		//convert duration to minutes
+		duration = Math.round(duration / 60000);
+		minutes = duration % 60;
+		duration = duration - minutes;
+		hours = duration / 60;
+		$duration.text(hours + ' hours ' + minutes + ' minutes');
+	};
+
+	//initialize vars
+	startDate = new Date();
+	tzOffset = startDate.getTimezoneOffset();
+	$startInput = $('#break-start');
+	startInput = $startInput[0];
+	endInput = $('#break-end')[0];
+	$duration = $('#duration');
+	//bind events
+	$startInput.on('change', startDateChange);
+	//start doing stuff
+	setInputDate(startInput, startDate);
+	startDateChange();
 });
