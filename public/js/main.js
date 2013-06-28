18d0a48ef6adc26ce24dd850beea6233daa3be4c
Michael Graybeal <michael@thegraybeals.com>
2013-06-27 22:14:36 -0600

    getting started on the inputs

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+$(function(){
+	var start
+	, now = new Date()
+	, findTheEnd;
 
+	findTheEnd = function(){
+		var milliseconds = 0;
+		
+	};
+
+	start = $('#break-start')[0];
+	start.valueAsNumber = now.getTime() - (now.getTimezoneOffset() * 60000);
+});
